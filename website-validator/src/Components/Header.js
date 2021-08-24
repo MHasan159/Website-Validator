e616f91602f25842bcde300548847ed6aa9fc82d
MohammadHasan <rony15987@yahoo.com>
2021-08-24 17:29:00 -0400

    added css and Header

--- website-validator/src/Components/Header.js
+++ website-validator/src/Components/Header.js
@@ -1,8 +1,37 @@
 
 // Contains our buttons on the top to navigate around app
-    //Home, SiteList, SiteRequest, Guidelines, dark/light mode
-const Header = () => {
+
+//Home, SiteList, SiteRequest, Guidelines, dark/light mode
+import React from "react";
+// import 'semantic-ui-css/semantic.min.css'
+// import { Button, Header } from 'semantic-ui-react'
+
+
+const Headers = ({isDarkMode, onDarkModeClick}) => {
+    return(
+        <div className = 'Header'>
+        <button >Home</button>
+        <button >SiteList</button>
+        <button >Guidelines</button>
+        
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? (
+            <span role="img" label="sun">
+              ☀️
+            </span>
+          ) : (
+            <span role="img" label="moon">
+              🌙
+            </span>
+          )}
+        </button>
+        <h1>Website Validator</h1> 
+
+
+        </div>
+    )
 
 }
 
-export default Header
+
+export default Headers;
--- website-validator/src/Components/Home.js
+++ website-validator/src/Components/Home.js
@@ -1,11 +1,16 @@
 
-
-
 //Description about app, featured verified websites.
 export default function Home() {
+
+
     return (
-        <div>
-            
-        </div>
+        <section>
+            <h2>Checkout some of these great websites!</h2>
+            <p>
+                Love to e-shop, but don't know if you can trust the site?
+                Don't worry! As fellow shoppers, we are here to help each
+                other make safe purchases.
+            </p>
+        </section>
     )
 }
