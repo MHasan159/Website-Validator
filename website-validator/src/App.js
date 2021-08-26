dca551b6f356a5c1aacc7a15139599acf4a31881
MohammadHasan <rony15987@yahoo.com>
2021-08-26 12:17:34 -0400

    test

--- website-validator/src/App.js
+++ website-validator/src/App.js
@@ -2,11 +2,12 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Header from "./Components/Header";
 import Home from "./Components/Home";
-import WebsiteList from "./Components/WebsiteList";
-import WebsiteDetails from "./Components/WebsiteDetails";
-import RequestForm from "./Components/RequestForm"
+// import WebsiteList from "./Components/WebsiteList";
+// import WebsiteDetails from "./Components/WebsiteDetails";
+// import RequestForm from "./Components/RequestForm"
 
 function App() {
+const [isDarkMode, setIsDarkMode] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/websites')
@@ -14,9 +15,19 @@ function App() {
     .then(webData => console.log(webData))
   }, [])
 
+  function handleDarkModeClick() {
+    setIsDarkMode(!isDarkMode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div div className={isDarkMode ? "App" : "App light"}>
+      <Header
+       isDarkMode={isDarkMode}
+       onDarkModeClick={handleDarkModeClick}
+      />
+      <Home/>
+      
+      
     </div>
   );
 }
--- website-validator/src/Components/Header.js
+++ website-validator/src/Components/Header.js
@@ -1,8 +1,37 @@
 
 // Contains our buttons on the top to navigate around app
-    //Home, SiteList, SiteRequest, Guidelines, dark/light mode
-const Header = () => {
+
+//Home, SiteList, SiteRequest, Guidelines, dark/light mode
+import React from "react";
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
+
+
+const Header = ({isDarkMode, onDarkModeClick}) => {
+    return(
+        <div>
+        <Button color = 'blue'>Home</Button>
+        <Button color = 'blue'>SiteList</Button>
+        <Button color = 'blue'>Guidelines</Button>
+        
+        <Button onClick={onDarkModeClick}>
+          {isDarkMode ? (
+            <span role="img" label="sun">
+              ☀️
+            </span>
+          ) : (
+            <span role="img" label="moon">
+              🌙
+            </span>
+          )}
+        </Button>
+        <Header color = 'green' as='h1'>Website Validator</Header> 
+
+
+        </div>
+    )
 
 }
 
-export default Header
+
+export default Header;
